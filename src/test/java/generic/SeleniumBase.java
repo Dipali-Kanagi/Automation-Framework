980a7476667ad6c96d05d0e8ff3e668990d4451b
inportianov <inportia_nov@yopmail.com>
2019-01-09 10:15:59 +0530

    added launchApp method in SeleniumBase

--- src/test/java/generic/SeleniumBase.java
+++ src/test/java/generic/SeleniumBase.java
@@ -1,5 +1,26 @@
 package generic;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumBase {
 
+	WebDriver driver;
+	
+	public void launchApplication(String browserName, String url) {
+		
+		if(browserName.equalsIgnoreCase("chrome")) {
+			
+			System.setProperty("", "");
+			driver = new ChromeDriver();
+			
+		}
+		else if(browserName.equalsIgnoreCase("firefox")) {
+			
+		}
+		else if(browserName.equalsIgnoreCase("ie")) {
+			
+		}
+	}
+	
 }
