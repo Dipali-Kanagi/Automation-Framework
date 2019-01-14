73ec8d1ba0dca25280e6f77df32561f57a7753d0
Dipali_Kanagi <dipali8kngi@gmail.com>
2019-01-14 10:34:24 +0530

    Added drivers folder and implemented selenium methods

--- src/test/java/generic/SeleniumBase.java
+++ src/test/java/generic/SeleniumBase.java
@@ -1,26 +1,72 @@
 package generic;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class SeleniumBase {
 
 	WebDriver driver;
-	
+	enum locator{id,name,className,tagName,linkText,partialLinkText,xpath,css};
 	public void launchApplication(String browserName, String url) {
 		
 		if(browserName.equalsIgnoreCase("chrome")) {
 			
-			System.setProperty("", "");
+			System.setProperty("webdriver.chrome.driver", "D:\\Apps\\eclipse-java-2018-09-win32-x86_64\\projects\\pom_automation_framework\\drivers\\chromedriver.exe");
 			driver = new ChromeDriver();
 			
 		}
 		else if(browserName.equalsIgnoreCase("firefox")) {
-			
+			System.setProperty("webdriver.gecko.driver", "D:\\Apps\\eclipse-java-2018-09-win32-x86_64\\projects\\pom_automation_framework\\drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
 		}
 		else if(browserName.equalsIgnoreCase("ie")) {
-			
+			System.setProperty("webdriver.ie.driver", "D:\\Apps\\eclipse-java-2018-09-win32-x86_64\\projects\\pom_automation_framework\\drivers\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
 		}
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+	}
+	public static void main(String args[]) {
+		SeleniumBase sb = new SeleniumBase();
+		sb.launchApplication("chrome", "http://naukri.com");
+		//sb.closeapplicaiton(true);
+	}
+	public void closeapplicaiton(boolean flag) {
+		if(flag) {
+		driver.quit();
+		}
+		else {
+			driver.close();
+
+		}
+	}
+	public void identifyElement(String address, locator type) {
+		 By by = null;
+		switch(type) {
+		case id:by = By.id(address);
+		break;
+		case className: by = By.className(address);
+		break;
+		case css: by = By.cssSelector(address);
+		break;
+		case linkText : by = By.linkText(address);
+		break;
+		case partialLinkText : by =By.partialLinkText(address);
+		break;
+		case xpath : by = By.xpath(address);
+		break;
+		case name : by = By.name(address);
+		break;
+		case tagName : by = By.tagName(address);
+		break;
+		
+		}
+		driver.findElement(by);
+				
 	}
 	
 }
